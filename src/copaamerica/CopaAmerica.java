f88d79c4df2fbcd93eaceee793bb50d697ff38e6
Juan Gomez <juan.camilo.mimo@gmail.com>
2015-04-15 21:43:16 -0500

    First commit

---
+++ src/copaamerica/CopaAmerica.java
@@ -0,0 +1,9 @@
+package copaamerica;
+import copaamerica.vista.Main;
+
+public class CopaAmerica {
+    public static void main(String args[]){
+        Main principal = new Main();
+        principal.setVisible(true);
+    }    
+}
